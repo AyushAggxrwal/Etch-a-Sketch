b92a19663cab47f10e12417c19ed73aba6b3313d
Ayush Aggarwal <ayushaggxrwal@gmail.com>
2023-07-09 05:30:21 +0530

    Create function to add squares as per User Input

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const container = document.querySelector('.container');
-for(let i = 1; i <= 16; i++){
-  for(let j = 1; j <= 16; j++){
-    const gridElement = document.createElement('div');
-    gridElement.classList.add('grid-element');
-    container.appendChild(gridElement);
-  }
+
+function resize(gridSize = 16) {
+  for(let i = 1; i <=  gridSize*gridSize; i++){
+      const gridElement = document.createElement('div');
+      gridElement.classList.add('grid-element');
+      container.appendChild(gridElement);
+      container.setAttribute('style', `grid-template-rows: repeat(${gridSize}, 1fr);
+      grid-template-columns: repeat(${gridSize}, 1fr);`)
+    }
 }
+
+resize();
+
+
+const slider = document.querySelector("#range-input")
+
+slider.addEventListener('mouseup', () => {
+  resize(slider.value);
+})
